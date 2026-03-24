e6ce6a8849d502be84815ac4a8e17f421892bf42
devill <3259300377@qq.com>
2026-03-24 19:24:53 +0800

    新增菜品和公共字段自动注入业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.sky.entity.Dish;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,4 +16,7 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    @Insert("insert into dish (name, category_id, price, image, description, create_time, update_time, create_user,update_user, status)\n" +
+            "        values (#{name}, #{categoryId}, #{price}, #{image}, #{description}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status}) ")
+    void saveWithDish(Dish dish);
 }
