112bfcd297f126863dada345e5574521c0d774c4
devill <3259300377@qq.com>
2026-04-14 15:21:24 +0800

    订单支付模块功能开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -14,4 +14,18 @@ public interface OrderMapper {
 
     @Select("select * from orders where id = #{id}")
     Orders getById(Long id);
+
+    /**
+     * 根据订单号和用户id查询订单
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
 }
